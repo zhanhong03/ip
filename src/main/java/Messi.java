78dd2c759682fe8bca5df85def46d4d9f46659f2
ZHANHONG\choyz <e1384444@u.nus.edu>
2026-02-05 18:35:58 +0800

    Finish up Level-0

--- src/main/java/Messi.java
+++ src/main/java/Messi.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Messi {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String horizontalLine = "____________________________________________________________";
         Scanner in = new Scanner(System.in);
         Task[] tasks = new Task[100];
@@ -20,27 +20,31 @@ public class Messi {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(horizontalLine);
                 break;
-            } else if (input.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + "." + tasks[i].toString());
-                }
-            } else if (input.startsWith("mark")) {
-                // Get the number after "mark "
-                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-                tasks[taskIndex].markAsDone();
-                System.out.println("Nice! I've marked this task as done:\n  " + tasks[taskIndex]);
-            } else if (input.startsWith("unmark")) {
-                // Get the number after "unmark "
-                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-                tasks[taskIndex].unmarkAsDone();
-                System.out.println("OK, I've marked this task as not done yet:\n  " + tasks[taskIndex]);
-            } else {
-                tasks[taskCount] = new Task(input);
-                taskCount++;
-                System.out.println("added: " + input);
             }
-            System.out.println(horizontalLine);
         }
     }
 }
+//            } else if (input.equals("list")) {
+//                System.out.println("Here are the tasks in your list:");
+//                for (int i = 0; i < taskCount; i++) {
+//                    System.out.println((i + 1) + "." + tasks[i].toString());
+//                }
+//            } else if (input.startsWith("mark")) {
+//                // Get the number after "mark "
+//                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+//                tasks[taskIndex].markAsDone();
+//                System.out.println("Nice! I've marked this task as done:\n  " + tasks[taskIndex]);
+//            } else if (input.startsWith("unmark")) {
+//                // Get the number after "unmark "
+//                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+//                tasks[taskIndex].unmarkAsDone();
+//                System.out.println("OK, I've marked this task as not done yet:\n  " + tasks[taskIndex]);
+//            } else {
+//                tasks[taskCount] = new Task(input);
+//                taskCount++;
+//                System.out.println("added: " + input);
+//            }
+//            System.out.println(horizontalLine);
+//        }
+//    }
+//}
